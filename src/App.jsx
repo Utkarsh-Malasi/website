0c58ed4294bfefee738014888a385b45735b8730
utkarshMalasi <utkarshmalasi2910@gmail.com>
2025-01-26 13:57:23 +0530

    hs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React, { useRef } from 'react'
-import Header from './Header.jsx'
-import './index.css'
-import Vision from './Vision.jsx'
-import Home from './Home.jsx'
-import Services from './Services.jsx'
-import Contact from './Contact.jsx'
-import Faq from './Faq.jsx'
+import React, { useRef } from 'react';
+import Header from './Header.jsx';
+import './index.css';
+import Vision from './Vision.jsx';
+import Home from './Home.jsx';
+import Services from './Services.jsx';
+import Contact from './Contact.jsx';
+import Faq from './Faq.jsx';
 
 function App() {
   const visionRef = useRef(null);
@@ -26,17 +26,15 @@ function App() {
        scrollToServices={() => scrollToSection(servicesRef)}
        scrollToFaq={() => scrollToSection(faqRef)}
        scrollToHome={() => scrollToSection(homeRef)}
-       
-       />
+      />
       <Home ref={homeRef}/>
-      <Vision  ref={visionRef}/>
+      <Vision ref={visionRef}/>
       <Services ref={servicesRef}/>
       <Contact ref={contactRef}/>
       <Faq ref={faqRef}/>
-
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
